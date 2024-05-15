837a5c585537c5201daacfcf9e4e97763fbb3107
Tiziano Morelli <tznmorelli@gmail.com>
2024-05-15 17:47:14 +0200

    Gestione delle distanze dei titoli e masthead e aggiunti bollini nell'area revisor

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -139,7 +139,7 @@
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
-        <div class="modal-header">
+        <div class="modal-header border-0">
           <h1 class="modal-title fs-5" id="exampleModalLabel">Cerca articolo</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
