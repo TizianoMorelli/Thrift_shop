736501aea6a4b7e9424621ecf59724741c19eb02
Giovanni Sugamiele <gionnymiele@MacBook-Pro-Elegion.local>
2024-05-16 20:07:25 +0200

    merge completato (forse)

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -15,54 +15,6 @@
             </div>
         </div>
     </div>
-<<<<<<< HEAD
-    @if ($announcement_to_check)
-    {{-- @dd($announcement_to_check->images) --}}
-    <div class="container my-5">
-        <div class="row justify-content-center ">
-            <div class="col-12 col-md-6 col-lg-4 my-4">
-                <div class="card mb-3">
-                    <img src=
-                    "@if ($announcement_to_check->images->count())
-                        {{ Storage::url($announcement_to_check->images->first()->path) }}
-                    @else 
-                        {{Storage::url('public/default-image.jpg')}}
-                    @endif" 
-                    class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <h4 class="card-title">{{$announcement_to_check->title}}</h4>
-                      <h6 class="card-text">{{$announcement_to_check->subtitle}}</h6>
-                      <p class="card-text">{{$announcement_to_check->body}}</p>
-                      <p class="card-text"><small class="text-body-secondary">Last updated {{$announcement_to_check->updated_at->format('d/m/Y')}}</small></p>
-                    </div>
-                    <div class="card-body">
-                        <div class="row justify-content-evenly">
-                            <div class="col-6">
-                                <form
-                                action="{{route('revisor.accept_announcement', ['announcement'=>$announcement_to_check])}}" method="POST">
-                                @csrf
-                                @method('PATCH')
-                                <button type="submit" class="btn btn-success">
-                                    Accetta
-                                </button>
-                                </form>
-                            </div>
-                            <div class="col-6 d-flex justify-content-end">
-                                <form
-                                action="{{route('revisor.reject_announcement', ['announcement'=>$announcement_to_check])}}" method="POST">
-                                @csrf
-                                @method('PATCH')
-                                <button type="submit" class="btn btn-success">
-                                    Rifiuta
-                                </button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                  </div>
-                  
-            </div>    
-=======
     @if($announcement_to_check)
     <div class="container my-5">
         <div class="row">
@@ -110,7 +62,6 @@
                     </tbody>
                 </table>
             </div>
->>>>>>> refs/remotes/origin/main
         </div>
     </div>
     @endif
