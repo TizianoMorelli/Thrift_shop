8c00f251f7e6601f913ad3b7960d1b77db813999
Dorian Poleni <dpoleni@yahoo.it>
2024-05-17 23:19:03 +0200

    il caricamento di un immagine funziona, ma ad ora è necessario gestire i job lanciando maunalmente il comando php artisan queue:work

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -3,13 +3,14 @@
 namespace App\Jobs;
 
 use Spatie\Image\Image;
-use Spatie\Image\Manipulations;
+use Spatie\Image\Enums\Fit;
 use Illuminate\Bus\Queueable;
+use Spatie\Image\Manipulations;
+use Spatie\Image\Enums\CropPosition;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Spatie\Image\Enums\CropPosition;
 
 class ResizeImage implements ShouldQueue
 {
@@ -36,13 +37,14 @@ class ResizeImage implements ShouldQueue
         $w = $this->w; //passo all'istanza le dimensioni dell'immagine
         $h = $this->h;
         // $srcPath = storage_path() . '/app/public/' . $this->path . '/' . $this->fileName; //percorso sorgente dell'immagine in storage/public/app
-        $srcPath = storage_path() . '/app/public' . $this->path . '/' . $this->fileName; //percorso sorgente dell'immagine in storage/public/app
+        $srcPath = storage_path() . '/app/public/' . $this->path . '/' . $this->fileName; //percorso sorgente dell'immagine in storage/public/app
         // $destPath = storage_path() . '/app/public/' . $this->path . "/crop_{$w}x{$h}_" . $this->fileName; //percorso di destinazione dell'immagine croppata
         $destPath = storage_path() . '/app/public/' . $this->path . "/crop_{$w}x{$h}_" . $this->fileName; //percorso di destinazione dell'immagine croppata
         
         $croppedImage = Image::load($srcPath)
                         // ->crop(Manipulations::CROP_CENTER, $w, $h) //! Non più funzionante
-                        ->crop($w, $h, CropPosition::Center) // Lancia la funzione di cropping con le dimensioni e la posizione data
+                        // ->crop($w, $h, CropPosition::Center) //!Diverso dal video - Lancia la funzione di cropping con le dimensioni e la posizione data
+                        ->fit(Fit::Crop,$w,$h)
                         ->save($destPath); //salva l'immagine nel percorso di destinazione
     }
 }
