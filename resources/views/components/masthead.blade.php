8c95ddb5342e07fcb5b1800f955ab9b34f9b8a71
Giovanni Sugamiele <gionnymiele@MacBook-Pro-Elegion.local>
2024-05-22 18:07:32 +0200

    masthead

--- resources/views/components/masthead.blade.php
+++ resources/views/components/masthead.blade.php
@@ -1,12 +1,12 @@
 <header class="masthead">
     <div class="container h-100">
       <div class="row h-100 justify-content-center justify-content-md-start  align-items-center">
-        <div class="col-10 col-md-5 bg-white p-4 rounded-3 ">
+        <div class="col-10 col-md-5 p-4 rounded-3 bg-white shadow_pers ">
           <h2 class="color-P my-2">
             {{__('header.cta')}}
           </h2>
 
-          <a class="btn btn_standard py-2 w-100 my-2" href="{{route('announcement.create')}}" >{{__('header.button')}}</a>
+          <a class="btn btn_standard py-2 w-100 my-2 shadow_pers" href="{{route('announcement.create')}}" >{{__('header.button')}}</a>
          
         </div>
       </div>
