f1e34902c316f5f51d26dc72c6924994fd40ac77
Tiziano Morelli <tznmorelli@gmail.com>
2024-05-22 16:29:30 +0200

    Sistemato front della pagina lavora con noi

--- lang/it/about.php
+++ lang/it/about.php
@@ -30,43 +30,35 @@ return [
 
     'abouttitle' => 'La nostra azienda',
 
-    'about1' => 'Fondata nel 1824, ThriftShop è nata con l\'obiettivo di rivoluzionare il modo in cui
-    le persone comprano e vendono beni e servizi online. Iniziando come una piccola startup con
-    una
-    visione ambiziosa, abbiamo lavorato instancabilmente per costruire una piattaforma che fosse
-    intuitiva, sicura e accessibile a tutti.',
+    'jobtitle' => 'Fare un lavoro che conta',
 
-    'about2' => 'Nel corso degli anni, abbiamo ampliato i nostri servizi per coprire una vasta gamma di
-    categorie,
-    dai
-    beni di consumo ai servizi professionali, rendendo più semplice per gli utenti trovare
-    esattamente
-    ciò di cui hanno bisogno. Grazie all\'impegno costante per la qualità e l\'affidabilità, siamo
-    diventati uno dei principali siti di annunci online, con milioni di utenti attivi ogni mese.',
+    'about1' => 'Fondata nel 2024, ThriftShop è nata con l\'obiettivo di rivoluzionare il modo in cui le persone comprano e vendono beni e servizi online. Iniziando come una piccola startup con una visione ambiziosa, abbiamo lavorato instancabilmente per costruire una piattaforma che fosse intuitiva, sicura e accessibile a tutti.',
 
-    'about3' => ' Il nostro successo si basa sulla fiducia che i nostri utenti ripongono in noi. Per garantire
-    che
-    ogni
-    annuncio sia conforme ai nostri standard di qualità, abbiamo introdotto un rigoroso processo
-    di
-    revisione. Ogni giorno, un team dedicato di revisori esamina gli annunci inviati per
-    assicurarsi che
-    rispettino le nostre linee guida, offrendo così un\'esperienza sicura e piacevole per tutti.',
+     'about2' => 'Puntiamo a fare della seconda mano la prima scelta. Al momento, l\'industria della moda contribuisce fino al 10% delle emissioni globali di CO2, ma riciclare abbigliamento su larga scala può ridurre l\'impatto climatico della moda al dettaglio. Con il vostro supporto, possiamo diminuire questa impronta.',
 
-    'about4' => 'Oltre a fornire una piattaforma di annunci, ci impegniamo a innovare costantemente,
-    migliorando le
-    funzionalità esistenti e introducendo nuove caratteristiche che rispondano alle esigenze dei
-    nostri
-    utenti. Siamo orgogliosi di creare una comunità vibrante dove persone da tutto il mondo
-    possono
-    connettersi, fare affari e crescere insieme.',
+    // 'about3' => ' Il nostro successo si basa sulla fiducia che i nostri utenti ripongono in noi. Per garantire
+    // che
+    // ogni
+    // annuncio sia conforme ai nostri standard di qualità, abbiamo introdotto un rigoroso processo
+    // di
+    // revisione. Ogni giorno, un team dedicato di revisori esamina gli annunci inviati per
+    // assicurarsi che
+    // rispettino le nostre linee guida, offrendo così un\'esperienza sicura e piacevole per tutti.',
 
-    'about5' => 'In ThriftShop, crediamo nel potere della condivisione e della collaborazione. La nostra
-    storia è
-    fatta di migliaia di storie di successo dei nostri utenti, e ogni giorno lavoriamo per
-    scrivere un
-    nuovo capitolo di questa avventura. Unisciti a noi e scopri cosa rende ThriftShop la scelta
-    preferita per gli annunci online.',
+    // 'about4' => 'Oltre a fornire una piattaforma di annunci, ci impegniamo a innovare costantemente,
+    // migliorando le
+    // funzionalità esistenti e introducendo nuove caratteristiche che rispondano alle esigenze dei
+    // nostri
+    // utenti. Siamo orgogliosi di creare una comunità vibrante dove persone da tutto il mondo
+    // possono
+    // connettersi, fare affari e crescere insieme.',
+
+    // 'about5' => 'In ThriftShop, crediamo nel potere della condivisione e della collaborazione. La nostra
+    // storia è
+    // fatta di migliaia di storie di successo dei nostri utenti, e ogni giorno lavoriamo per
+    // scrivere un
+    // nuovo capitolo di questa avventura. Unisciti a noi e scopri cosa rende ThriftShop la scelta
+    // preferita per gli annunci online.',
 
 
 
